50e88bf1b63552f4776531f16d921afeacbdf77a
Mark Rudolph <mark_rudolph@yahoo.com>
2018-07-19 08:29:06 -0700

    initial commit - uses truffle ganache and web3 0.20.*

---
+++ README.md
@@ -0,0 +1,26 @@
+# btc_marketcap
+
+# Setting up
+# NOTE:as of July 20 2018 web3 1.0.0* fails to install?! 
+# instead uses web3 ^0.20.6 - if necessary install 0.20.* by the following:
+# npm install web@^0.20.0  
+# there is a package-web3-1.0.0beta24.json for later use of web3 ^1.0.0
+# NOTE: web3 ^1.0.0 supports websocket provider
+```
+download truffle ganache (test blockchain replaces test-rpc)
+npm install -g truffle 
+npm install 
+start ganache
+truffle compile
+truffle migrate
+```
+
+# Running marketcap oracle
+```
+node oracle.js
+```
+
+# Running client - initially run twice to get first update 
+```
+node client.js
+```
---
+++ contracts/CMCOracle.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.4.24;
+
+contract CMCOracle {
+  // Contract owner
+  address public owner;
+
+  // BTC Marketcap Storage
+  uint public btcMarketCap;
+
+  // Callback function
+  event CallbackGetBTCCap();
+
+
+  constructor() public {
+    owner = msg.sender;
+  }
+
+  function updateBTCCap() public {
+    // Calls the callback function
+    emit CallbackGetBTCCap();
+  }
+
+  function setBTCCap(uint cap) public {
+    // If it isn't sent by a trusted oracle
+    // a.k.a ourselves, ignore it
+    require(msg.sender == owner);
+    btcMarketCap = cap;
+  }
+
+  function getBTCCap() constant public returns (uint) {
+    return btcMarketCap;
+  }
+}
